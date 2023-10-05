0c9a98eecb06d2bd3da07af0cdaaab4a42389e97
NikhilD1998 <nikhildadab@hotmail.com>
2023-10-05 11:30:56 +0530

    Added Intl

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,16 @@
 import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
 
 final uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+}
 
 class Expense {
   Expense({
